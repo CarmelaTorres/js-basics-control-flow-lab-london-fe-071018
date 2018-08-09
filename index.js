1fca2f0461ccc24ade72731fef1e7f8846c8dfde
Carmela Torres Molleda <carmela.torres@wework.com>
2018-08-09 19:40:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function scuberGreetingForFeet(distance) {
   else if (distance >= 2000){
     return 'I will gladly take your thirty bucks.'
   }
-  if (distance > 2500){
+  else (distance > 2500){
     return 'No can do.'
   }
 }
