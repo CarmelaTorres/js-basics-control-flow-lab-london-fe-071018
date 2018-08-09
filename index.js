0e414a971c126bbbeaf24d45664f1f560a67ef86
Carmela Torres Molleda <carmela.torres@wework.com>
2018-08-09 19:32:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code in this file!
 
 function scuberGreetingForFeet(distance) {
-  if (distance <= 199); {
+  if (distance <= 199 && distance <=400); {
     return "This is on me!";
   }
 }
