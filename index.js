7c8c168a4668e5d1a158d92303fb5970905b847b
Carmela Torres Molleda <carmela.torres@wework.com>
2018-08-09 19:22:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 // Write your code in this file!
 
-function scuberGreetingForFeet()
-
-if (A = 199
-
- if (weatherToday === "Rainy") {
-    return `${dogName} did not exercise due to rain`;
-
+function scuberGreetingForFeet(distance) {
+  if (distance <= 199);
+}
